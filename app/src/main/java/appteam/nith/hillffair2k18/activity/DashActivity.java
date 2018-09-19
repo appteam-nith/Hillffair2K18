2ae9fc305ad7cc6e925c218d60fe5fd08138bc54
thisisnsh <hadanis.singh@gmail.com>
2018-09-20 01:26:30 +0530

    Added: NewAdapter

---
+++ app/src/main/java/appteam/nith/hillffair2k18/activity/DashActivity.java
@@ -0,0 +1,17 @@
+package appteam.nith.hillffair2k18.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import appteam.nith.hillffair2k18.R;
+
+public class DashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash);
+
+    }
+
+}
