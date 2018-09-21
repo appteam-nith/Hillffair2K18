40e21fcd915d9ae801f1928873c46ea4200f7162
rohannawathe <rohannawathe@hotmail.com>
2018-09-22 03:42:58 +0530

    Added Layouts for Quiz and Quiz Map. Also created outline for the code behind the activities

---
+++ app/src/main/java/appteam/nith/hillffair2k18/activity/QuizMap.java
@@ -0,0 +1,13 @@
+package appteam.nith.hillffair2k18.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_map);
+    }
+}
