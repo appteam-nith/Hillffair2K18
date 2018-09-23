d2b0c96bc52bd7efd95df011d0fef6fc7cbdae39
thisisnsh <hadanis.singh@gmail.com>
2018-09-24 00:23:06 +0530

    Added: NavDrawer

--- app/src/main/java/appteam/nith/hillffair2k18/activity/DashActivity.java
+++ app/src/main/java/appteam/nith/hillffair2k18/activity/DashActivity.java
@@ -10,8 +10,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SnapHelper;
-import android.view.Gravity;
 import android.view.View;
 import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.RelativeLayout;
@@ -19,7 +17,6 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
-import com.github.rubensousa.gravitysnaphelper.GravitySnapHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,9 +42,10 @@ public class DashActivity extends AppCompatActivity {
     private ScrollAdapter scrollAdapter;
     private List<Scroll> scrollList = new ArrayList<>();
     private ScrollView scrollView;
-    private TextView title;
+    private TextView title, profileNav, aboutNav, settingNav, sponsorNav;
     private RelativeLayout linearLayout;
     private boolean check = true;
+    private RelativeLayout navDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +62,11 @@ public class DashActivity extends AppCompatActivity {
         profile = findViewById(R.id.profile);
         nav = findViewById(R.id.nav);
         navAnim = findViewById(R.id.navAnim);
+        navDrawer = findViewById(R.id.navDrawer);
+        profileNav = findViewById(R.id.profileNav);
+        aboutNav = findViewById(R.id.aboutNav);
+        settingNav = findViewById(R.id.settingNav);
+        sponsorNav = findViewById(R.id.sponsorNav);
         viewPager = findViewById(R.id.viewpager);
 
         final SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager(), DashActivity.this);
@@ -74,7 +77,7 @@ public class DashActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (check) {
                     ValueAnimator valueAnimator = ValueAnimator.ofFloat(0f, 1f);
-                    valueAnimator.setDuration(1500);
+                    valueAnimator.setDuration(1000);
                     valueAnimator.setInterpolator(new AnticipateOvershootInterpolator());
                     valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                         @Override
@@ -85,6 +88,75 @@ public class DashActivity extends AppCompatActivity {
                     });
                     valueAnimator.start();
                     check = false;
+                    navDrawer.setVisibility(View.VISIBLE);
+                    profileNav.setVisibility(View.VISIBLE);
+                    aboutNav.setVisibility(View.VISIBLE);
+                    settingNav.setVisibility(View.VISIBLE);
+                    sponsorNav.setVisibility(View.VISIBLE);
+                    ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(navDrawer, "alpha", 0, 1);
+                    ObjectAnimator objectAnimator1 = ObjectAnimator.ofFloat(recyclerView, "alpha", 1, 0);
+
+                    ObjectAnimator objectAnimator2 = ObjectAnimator.ofFloat(aboutNav, "alpha", 0, 1);
+                    ObjectAnimator objectAnimator3 = ObjectAnimator.ofFloat(profileNav, "alpha", 0, 1);
+                    ObjectAnimator objectAnimator4 = ObjectAnimator.ofFloat(settingNav, "alpha", 0, 1);
+                    ObjectAnimator objectAnimator5 = ObjectAnimator.ofFloat(sponsorNav, "alpha", 0, 1);
+
+                    ObjectAnimator objectAnimator6 = ObjectAnimator.ofFloat(aboutNav, "translationY", 15f, 0f);
+                    ObjectAnimator objectAnimator7 = ObjectAnimator.ofFloat(profileNav, "translationY", 15f, 0f);
+                    ObjectAnimator objectAnimator8 = ObjectAnimator.ofFloat(settingNav, "translationY", 15f, 0f);
+                    ObjectAnimator objectAnimator9 = ObjectAnimator.ofFloat(sponsorNav, "translationY", 15f, 0f);
+
+                    objectAnimator5.setDuration(700);
+                    objectAnimator4.setDuration(700);
+                    objectAnimator3.setDuration(700);
+                    objectAnimator6.setDuration(700);
+                    objectAnimator7.setDuration(700);
+                    objectAnimator8.setDuration(700);
+                    objectAnimator9.setDuration(700);
+                    objectAnimator2.setDuration(700);
+                    objectAnimator1.setDuration(500);
+                    objectAnimator.setDuration(500);
+
+                    objectAnimator2.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator3.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator4.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator5.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator6.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator7.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator8.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator9.setInterpolator(new AnticipateOvershootInterpolator());
+
+                    AnimatorSet animatorSet = new AnimatorSet();
+                    animatorSet.playTogether(objectAnimator, objectAnimator1, objectAnimator2, objectAnimator3, objectAnimator4, objectAnimator5, objectAnimator6, objectAnimator7, objectAnimator8, objectAnimator9);
+                    animatorSet.start();
+                    animatorSet.addListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            navDrawer.setVisibility(View.VISIBLE);
+                            recyclerView.setVisibility(View.GONE);
+                            profileNav.setVisibility(View.VISIBLE);
+                            aboutNav.setVisibility(View.VISIBLE);
+                            settingNav.setVisibility(View.VISIBLE);
+                            sponsorNav.setVisibility(View.VISIBLE);
+
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+
+                        }
+                    });
+
                 } else {
                     ValueAnimator valueAnimator = ValueAnimator.ofFloat(0f, 1f);
                     valueAnimator.setDuration(1500);
@@ -97,6 +169,73 @@ public class DashActivity extends AppCompatActivity {
                     });
                     valueAnimator.start();
                     check = true;
+                    recyclerView.setVisibility(View.VISIBLE);
+                    ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(navDrawer, "alpha", 1, 0);
+                    ObjectAnimator objectAnimator1 = ObjectAnimator.ofFloat(recyclerView, "alpha", 0, 1);
+
+                    ObjectAnimator objectAnimator2 = ObjectAnimator.ofFloat(aboutNav, "alpha", 1, 0);
+                    ObjectAnimator objectAnimator3 = ObjectAnimator.ofFloat(profileNav, "alpha", 1, 0);
+                    ObjectAnimator objectAnimator4 = ObjectAnimator.ofFloat(settingNav, "alpha", 1, 0);
+                    ObjectAnimator objectAnimator5 = ObjectAnimator.ofFloat(sponsorNav, "alpha", 1, 0);
+
+                    ObjectAnimator objectAnimator6 = ObjectAnimator.ofFloat(aboutNav, "translationY", 0f, 15f);
+                    ObjectAnimator objectAnimator7 = ObjectAnimator.ofFloat(profileNav, "translationY", 0f, 15f);
+                    ObjectAnimator objectAnimator8 = ObjectAnimator.ofFloat(settingNav, "translationY", 0f, 15f);
+                    ObjectAnimator objectAnimator9 = ObjectAnimator.ofFloat(sponsorNav, "translationY", 0f, 15f);
+
+                    objectAnimator.setDuration(500);
+                    objectAnimator5.setDuration(500);
+                    objectAnimator1.setDuration(500);
+                    objectAnimator4.setDuration(500);
+                    objectAnimator3.setDuration(500);
+                    objectAnimator6.setDuration(500);
+                    objectAnimator7.setDuration(500);
+                    objectAnimator8.setDuration(500);
+                    objectAnimator9.setDuration(500);
+                    objectAnimator2.setDuration(500);
+
+                    objectAnimator2.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator3.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator4.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator5.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator6.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator7.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator8.setInterpolator(new AnticipateOvershootInterpolator());
+                    objectAnimator9.setInterpolator(new AnticipateOvershootInterpolator());
+
+                    objectAnimator1.setStartDelay(350);
+                    objectAnimator.setStartDelay(350);
+
+                    AnimatorSet animatorSet = new AnimatorSet();
+                    animatorSet.playTogether(objectAnimator, objectAnimator1, objectAnimator2, objectAnimator3, objectAnimator4, objectAnimator5, objectAnimator6, objectAnimator7, objectAnimator8, objectAnimator9);
+                    animatorSet.start();
+                    animatorSet.addListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            recyclerView.setVisibility(View.VISIBLE);
+                            navDrawer.setVisibility(View.GONE);
+                            profileNav.setVisibility(View.GONE);
+                            aboutNav.setVisibility(View.GONE);
+                            settingNav.setVisibility(View.GONE);
+                            sponsorNav.setVisibility(View.GONE);
+
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+
+                        }
+                    });
                 }
             }
         });
