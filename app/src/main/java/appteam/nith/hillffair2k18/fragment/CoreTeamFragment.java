d52600472792cbb807645a2bae2e7468b440fd15
Abhinavlamba <abhinavlamba826@gmail.com>
2018-09-26 04:23:20 +0530

    Available API Linked

--- app/src/main/java/appteam/nith/hillffair2k18/fragment/CoreTeamFragment.java
+++ app/src/main/java/appteam/nith/hillffair2k18/fragment/CoreTeamFragment.java
@@ -1,5 +1,4 @@
 package appteam.nith.hillffair2k18.fragment;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,17 +9,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import appteam.nith.hillffair2k18.R;
 import appteam.nith.hillffair2k18.adapter.TeamAdapter;
 import appteam.nith.hillffair2k18.model.Team;
-
+import appteam.nith.hillffair2k18.model.Wall;
 /**
  * Coded by ThisIsNSH on Someday.
  */
-
 public class CoreTeamFragment extends Fragment {
 
     private RecyclerView recyclerView;
@@ -55,10 +61,37 @@ public class CoreTeamFragment extends Fragment {
 
     public void getData() {
         teamList.clear();
-        teamList.add(new Team("Captaion Marvel", "https://www.hdwallpapersfreedownload.com/uploads/large/super-heroes/captain-marvel-avengers-brie-larson-super-hero-hd-wallpaper.jpg", "Chief"));
-        teamList.add(new Team("Thanos", "https://pre00.deviantart.net/db91/th/pre/i/2017/197/8/0/thanos_wallpaper_16_by_rippenstain-dbghpzw.jpg", "Villan"));
-        teamList.add(new Team("Iron Mam", "https://wallpapersite.com/images/pages/ico_n/15263.jpg", "Hero"));
-        teamAdapter.notifyDataSetChanged();
+//        teamList.add(new Team("Captaion Marvel", "https://www.hdwallpapersfreedownload.com/uploads/large/super-heroes/captain-marvel-avengers-brie-larson-super-hero-hd-wallpaper.jpg", "Chief"));
+//        teamList.add(new Team("Thanos", "https://pre00.deviantart.net/db91/th/pre/i/2017/197/8/0/thanos_wallpaper_16_by_rippenstain-dbghpzw.jpg", "Villan"));
+//        teamList.add(new Team("Iron Mam", "https://wallpapersite.com/images/pages/ico_n/15263.jpg", "Hero"));
+//        teamAdapter.notifyDataSetChanged();
+        AndroidNetworking.get("http://hillffair.tk/getcoreteam")
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // do anything with response
+                        try {
+                            int users = response.length();
+                            for (int i = 0;i<users;i++) {
+                                JSONObject json = response.getJSONObject(i);
+                                String name = json.getString("name");
+                                String profile = json.getString("profile_pic");
+                                String position = json.getString("position");
+                                teamList.add(new Team(name, "https://www.hdwallpapersfreedownload.com/uploads/large/super-heroes/captain-marvel-avengers-brie-larson-super-hero-hd-wallpaper.jpg" , position));
+                            }
+                            teamAdapter.notifyDataSetChanged();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                    }
+                });
+
 
     }
 
--- app/src/main/java/appteam/nith/hillffair2k18/fragment/LeaderboardFragment.java
+++ app/src/main/java/appteam/nith/hillffair2k18/fragment/LeaderboardFragment.java
@@ -3,20 +3,39 @@ package appteam.nith.hillffair2k18.fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import appteam.nith.hillffair2k18.R;
+import appteam.nith.hillffair2k18.adapter.ClubAdapter;
+import appteam.nith.hillffair2k18.adapter.LeaderboardAdapter;
+import appteam.nith.hillffair2k18.model.Club;
+import appteam.nith.hillffair2k18.model.Leaderboard;
 
 /**
  * Coded by ThisIsNSH on Someday.
  */
 
 public class LeaderboardFragment extends Fragment implements View.OnClickListener {
-
+    private LeaderboardAdapter clubAdapter;
+    private RecyclerView recyclerView;
+    private List<Leaderboard> clubList = new ArrayList<>();
     private TextView point, referral, popular;
     private Activity activity;
 
@@ -35,19 +54,54 @@ public class LeaderboardFragment extends Fragment implements View.OnClickListene
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_leaderboard, container, false);
-        getData();
+
         point = view.findViewById(R.id.point);
         referral = view.findViewById(R.id.referral);
         popular = view.findViewById(R.id.popular);
         popular.setOnClickListener(this);
         point.setOnClickListener(this);
         referral.setOnClickListener(this);
+        recyclerView = view.findViewById(R.id.leaderboard);
+        clubAdapter = new LeaderboardAdapter(clubList, activity);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(clubAdapter);
+        recyclerView.setNestedScrollingEnabled(false);
+        getData();
         Log.e("LeaderFragment", "onCreateView: ");
         return view;
     }
 
     public void getData() {
+
+        clubList.clear();
+        AndroidNetworking.get("http://hillffair.tk/getleaderboard")
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            int users = response.length();
+                            for (int i = 0;i<users;i++) {
+                                JSONObject json = response.getJSONObject(i);
+                                String clubname = json.getString("name");
+                                String score = json.getString("score");
+                                String id = json.getString("id");
+                                clubList.add(new Leaderboard(clubname,id, score));
+                            }
+                            clubAdapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                    }
+                });
+
+        clubAdapter.notifyDataSetChanged();
 
     }
 
